a59f84140e2aadb55b4e4afbbe0892159c3155ac
cbarros7 <cj.barros07@gmail.com>
2020-08-14 21:36:46 -0500

    Update task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # task advance with pupppet
 exec { 'http header':
-  command  => 'sudo apt-get update -y;
+    command  => 'sudo apt-get update -y;
 	sudo apt-get install nginx -y;
 	sudo sed -i "/servername /a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
 	sudo service nginx restart',
-  provider => shell,
+    provider => shell,
 }
