410848c531111e1c6e11bccc3aec4322766b1afc
cbarros7 <cj.barros07@gmail.com>
2020-09-17 13:06:49 -0500

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+    Prints the titles of the first 10 hot
+    posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+        Top ten titles
+    """
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Carlos Barros'}
+    _size = {"limit": 10}
+    r = requests.get(base_url, params=_size, headers=headers).json()
+    child = r.get('data', {}).get('children')
+    for results in child:
+        print(results.get('data').get('title'))
